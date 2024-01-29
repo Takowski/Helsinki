17192e0fcdf1da36944b04d02329e4aa401a1f2a
Takowski <93830775+Takowski@users.noreply.github.com>
2024-01-29 10:06:37 +0100

    1.6 15min

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,14 +6,29 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const setToGood = (newGood) => {
+    console.log('good now',newGood)
+    setGood(newGood)
+  } 
+  const setToNeutral = (newNeutral) => {
+    console.log('neutral now',newNeutral)
+    setNeutral(newNeutral)
+  } 
+  const setToBad = (newBad) => {
+    console.log('bad now',newBad)
+    setBad(newBad)
+  } 
+
   return (
     <div>
       <h1>Give Feedback</h1>
-      <button>Good</button><button>neutral</button><button>bad</button>
+      <button onClick={() =>setToGood(good +1)}>Good</button>
+      <button onClick={() =>setToNeutral(neutral +1)}>neutral</button>
+      <button onClick={() =>setToBad(bad +1)}>bad</button>
       <h1>Statistics</h1>
-      <p>Good 0</p>
-      <p>Neutral 0</p>
-      <p>Bad 0</p>
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
     </div>
   )
 }
